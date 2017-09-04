0382778c686dd8b4d0099e1c3abe782aaebdfb43
justWarlow <31121824+justWarlow@users.noreply.github.com>
2017-09-04 15:35:35 -0400

    Update 341.java

--- 341.java
+++ 341.java
@@ -1,1 +1,27 @@
-public class NestedIterator implements Iterator<Integer> {    Stack<Iterator<NestedInteger>> st;    NestedInteger cur;    public NestedIterator(List<NestedInteger> nestedList) {        st = new Stack();        st.push(nestedList.iterator());    }    @Override    public Integer next() {        return cur.getInteger();    }    @Override    public boolean hasNext() {        do {            while (!st.empty() && !st.peek().hasNext()) st.pop();            if (st.empty()) return false;            for (cur = st.peek().next(); !cur.isInteger(); cur = st.peek().next()) {                st.push(cur.getList().iterator());                if (!st.peek().hasNext()) break;            }         } while (!cur.isInteger());        return true;    }}
+public class NestedIterator implements Iterator<Integer> {
+    Stack<Iterator<NestedInteger>> st;
+    NestedInteger cur;
+
+    public NestedIterator(List<NestedInteger> nestedList) {
+        st = new Stack();
+        st.push(nestedList.iterator());
+    }
+
+    @Override
+    public Integer next() {
+        return cur.getInteger();
+    }
+
+    @Override
+    public boolean hasNext() {
+        do {
+            while (!st.empty() && !st.peek().hasNext()) st.pop();
+            if (st.empty()) return false;
+            for (cur = st.peek().next(); !cur.isInteger(); cur = st.peek().next()) {
+                st.push(cur.getList().iterator());
+                if (!st.peek().hasNext()) break;
+            } 
+        } while (!cur.isInteger());
+        return true;
+    }
+}
