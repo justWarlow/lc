6f099f1be3eaccbcce184ab47997230edc299866
justWarlow <31121824+justWarlow@users.noreply.github.com>
2017-09-04 15:32:29 -0400

    Update 334.java

--- 334.java
+++ 334.java
@@ -1,1 +1,19 @@
-class Solution {    public boolean increasingTriplet(int[] nums) {        if(nums.length < 3) return false;        int min = nums[0];        long diff = Integer.MAX_VALUE;        for(int i =  1;i<nums.length;i++){            if(nums[i]>min){                if(nums[i]-min > diff) return true;                diff = nums[i]-min;                continue;            }            int d = min - nums[i];            diff+=d;            min = nums[i];        }        return false;    }}
+class Solution {
+  
+    public boolean increasingTriplet(int[] nums) {
+        if(nums.length < 3) return false;
+        int min = nums[0];
+        long diff = Integer.MAX_VALUE;
+        for(int i =  1;i<nums.length;i++){
+            if(nums[i]>min){
+                if(nums[i]-min > diff) return true;
+                diff = nums[i]-min;
+                continue;
+            }
+            int d = min - nums[i];
+            diff+=d;
+            min = nums[i];
+        }
+        return false;
+    }
+}
