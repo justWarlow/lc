9c69ec295b5c1568a62c0017f70930b43bf7c368
justWarlow <31121824+justWarlow@users.noreply.github.com>
2017-09-05 00:58:08 -0400

    Update 387.java

--- 387.java
+++ 387.java
@@ -1,1 +1,24 @@
-class Solution {    public int firstUniqChar(String s) {        char[] schar = s.toCharArray();        int result = schar.length;        int[] seen = new int[26];        Arrays.fill(seen, -1);        for (int i = 0; i < schar.length; i++) {            int index = schar[i]-'a';            if (seen[index] == -1) {                seen[index] = i;            }            else if (seen[index] >= 0) {                        seen[index] = -2;                     }        }        for (int i = 0; i < 26; i++) {            if (seen[i] >= 0) {                result = Math.min(result, seen[i]);            }        }        return result == schar.length?-1:result;    }}
+class Solution {
+    public int firstUniqChar(String s) {
+        char[] schar = s.toCharArray();
+        int result = schar.length;
+        int[] seen = new int[26];
+        Arrays.fill(seen, -1);
+        for (int i = 0; i < schar.length; i++) {
+            int index = schar[i]-'a';
+            if (seen[index] == -1) {
+                seen[index] = i;
+            }
+            else if (seen[index] >= 0) {
+                        seen[index] = -2;
+                     }
+        }
+        for (int i = 0; i < 26; i++) {
+            if (seen[i] >= 0) {
+                result = Math.min(result, seen[i]);
+            }
+        }
+        return result == schar.length?-1:result;
+    }
+  
+}
