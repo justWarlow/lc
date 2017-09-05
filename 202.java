9f064a7ca90223663f1c8a565910ab56807530e2
justWarlow <31121824+justWarlow@users.noreply.github.com>
2017-09-05 00:54:59 -0400

    Update 202.java

--- 202.java
+++ 202.java
@@ -1,1 +1,19 @@
-class Solution {    public boolean isHappy(int n) {        if (n <= 0) {            return false;        }        HashSet<Integer> set = new HashSet<Integer>();        while (n !=1 && !set.contains(n)) {            set.add(n);            int m = n;            n = 0;            while (m != 0) {                n += (m%10)*(m%10);                m = m/10;            }        }        return n == 1;    }}
+class Solution {
+  
+    public boolean isHappy(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        HashSet<Integer> set = new HashSet<Integer>();
+        while (n !=1 && !set.contains(n)) {
+            set.add(n);
+            int m = n;
+            n = 0;
+            while (m != 0) {
+                n += (m%10)*(m%10);
+                m = m/10;
+            }
+        }
+        return n == 1;
+    }
+}
