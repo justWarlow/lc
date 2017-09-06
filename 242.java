8a0255350995b30375f54fdc6bc972a74197ba5c
justWarlow <31121824+justWarlow@users.noreply.github.com>
2017-09-06 08:20:36 -0400

    Update 242.java

--- 242.java
+++ 242.java
@@ -1,1 +1,24 @@
-class Solution {    public boolean isAnagram(String s, String t) {        if (s == null && t == null) {            return true;        }        if (s == null || t == null) {            return false;        }        if (s.length() != t.length()) {            return false;        }        int[] ct= new int[26];              for (int i = 0; i < s.length(); i++) {            ct[s.charAt(i) - 'a']++;        }        for (int i = 0; i < t.length(); i++) {            ct[t.charAt(i) - 'a']--;            if (ct[t.charAt(i) - 'a'] < 0) {                return false;            }        }        return true;    }}
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s == null && t == null) {
+            return true;
+        }
+        if (s == null || t == null) {
+            return false;
+        }
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] ct= new int[26];      
+        for (int i = 0; i < s.length(); i++) {
+            ct[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            ct[t.charAt(i) - 'a']--;
+            if (ct[t.charAt(i) - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
