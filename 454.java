cdd12c165f08bac4e3a0a86e53e1323f5f6497e6
justWarlow <31121824+justWarlow@users.noreply.github.com>
2017-09-07 00:24:28 -0400

    Update 454.java

--- 454.java
+++ 454.java
@@ -1,1 +1,17 @@
-class Solution {    public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {        Map<Integer, Integer> map = new HashMap<Integer , Integer>();        Map<Integer, Integer> map2 = new HashMap<Integer , Integer>();        for (int i = 0; i<A.length; i++) {            for (int j = 0; j<A.length; j++) {        map.put(A[i] + B[j], map.getOrDefault(A[i] + B[j], 0) + 1);   			        map2.put(C[i] + D[j], map2.getOrDefault(C[i] + D[j], 0) + 1);    			}}	        int res=0;	        for (int i : map.keySet()) {res += map.get(i) * map2.getOrDefault(-i, 0);	}	      return res;            }}
+class Solution {
+    public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+        Map<Integer, Integer> map1 = new HashMap<Integer , Integer>();
+        Map<Integer, Integer> map2 = new HashMap<Integer , Integer>();
+        for (int i = 0; i<A.length; i++) {
+            for (int j = 0; j<A.length; j++) {
+              map1.put(A[i] + B[j], map1.getOrDefault(A[i] + B[j], 0) + 1);
+              map2.put(C[i] + D[j], map2.getOrDefault(C[i] + D[j], 0) + 1);
+              }
+          }
+	        int res=0;
+	        for (int i : map1.keySet()) {
+            res += map1.get(i) * map2.getOrDefault(-i, 0);
+          }
+	      return res;        
+    }
+}
