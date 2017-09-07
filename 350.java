8960794a112c3e2d04628f1ecd746fe212b6517d
justWarlow <31121824+justWarlow@users.noreply.github.com>
2017-09-07 00:21:28 -0400

    Update 350.java

--- 350.java
+++ 350.java
@@ -1,1 +1,22 @@
-class Solution {    public int[] intersect(int[] nums1, int[] nums2) {        Arrays.sort(nums1);        Arrays.sort(nums2);         int i = 0, j = 0, counter = 0;        int iL = nums1.length;        int jL = nums2.length;        int[] res = new int [iL < jL ? iL : jL];                while (i < iL && j < jL) {            if (nums1[i] == nums2[j]) {                results[counter++] = nums1[i];                i++;                j++;            } else if (nums1[i] > nums2[j]) {                j++;            } else {                i++;            }        }        return Arrays.copyOf(results, counter);    }}
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2); 
+        int i = 0, j = 0, counter = 0;
+        int iL = nums1.length;
+        int jL = nums2.length;
+        int[] res = new int [iL < jL ? iL : jL];        
+        while (i < iL && j < jL) {
+            if (nums1[i] == nums2[j]) {
+                results[counter++] = nums1[i];
+                i++;
+                j++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                i++;
+            }
+        }
+        return Arrays.copyOf(results, counter);
+    }
+}
