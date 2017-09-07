f7a380555781045faa10660055e6eca61913bd63
justWarlow <31121824+justWarlow@users.noreply.github.com>
2017-09-07 00:36:36 -0400

    Update 238.java

--- 238.java
+++ 238.java
@@ -1,1 +1,15 @@
-class Solution {    public int[] productExceptSelf(int[] nums) {        int n = nums.length;        int[] res = new int[n];               res[n - 1] = 1;        for (int i = n - 2; i >= 0; --i) {            res[i] = res[i + 1] * nums[i + 1];        }               for (int i = 0; i < n - 1; ++i) {            res[i] = res[i] * res[n - 1];            res[n - 1] = res[n - 1] * nums[i];        }           return res;    }}
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];       
+        res[n - 1] = 1;
+        for (int i = n - 2; i >= 0; --i) {
+            res[i] = res[i + 1] * nums[i + 1];
+        }       
+        for (int i = 0; i < n - 1; ++i) {
+            res[i] = res[i] * res[n - 1];
+            res[n - 1] = res[n - 1] * nums[i];
+        }   
+        return res;
+    }
+}
