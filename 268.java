18476ae1a80caf463eec7c21e9bc9b2f0ba44fc7
justWarlow <31121824+justWarlow@users.noreply.github.com>
2017-09-08 08:33:56 -0400

    08/09/2017

---
+++ 268.java
@@ -0,0 +1,1 @@
+class Solution {    public int missingNumber(int[] nums) {        int sum = nums.length;        for (int i = 0; i < nums.length; i++) {            sum -= nums[i] + i;        }         return sum;}}
