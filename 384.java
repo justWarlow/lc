5ddf0d274671f50da2a10b47dcbe1412d8478bfd
justWarlow <31121824+justWarlow@users.noreply.github.com>
2017-09-08 08:38:12 -0400

    Update 384.java

--- 384.java
+++ 384.java
@@ -1,1 +1,41 @@
-class Solution {    int[] original;    int[] shuffled;    int size;    Random rand = new Random();     public Solution(int[] nums) {        original = nums;        size = nums.length;        shuffled = new int[size];        for (int i = 0; i<size; i++){            shuffled[i] = original[i];        }    }        /** Resets the array to its original configuration and return it. */    public int[] reset() {        return original;    }        /** Returns a random shuffling of the array. */    void swap(int[] nums, int x, int y){        int temp = nums[x];        nums[x] = nums[y];        nums[y] = temp;    }        public int[] shuffle() {        int count=size;        while ( count > 0) {            swap(shuffled, count-1, rand.nextInt(count--));        }        return shuffled;    }}/** * Your Solution object will be instantiated and called as such: * Solution obj = new Solution(nums); * int[] param_1 = obj.reset(); * int[] param_2 = obj.shuffle(); */
+class Solution {
+    int[] original;
+    int[] shuffled;
+    int size;
+    Random rand = new Random(); 
+    public Solution(int[] nums) {
+        original = nums;
+        size = nums.length;
+        shuffled = new int[size];
+        for (int i = 0; i < size; i++) {
+            shuffled[i] = original[i];
+        }
+    }
+    
+    /** Resets the array to its original configuration and return it. */
+    public int[] reset() {
+        return original;
+    }
+    
+    /** Returns a random shuffling of the array. */
+    void swap(int[] nums, int x, int y){
+        int temp = nums[x];
+        nums[x] = nums[y];
+        nums[y] = temp;
+    }
+    
+    public int[] shuffle() {
+        int count = size;
+        while ( count > 0) {
+            swap(shuffled, count - 1, rand.nextInt(count--));
+        }
+        return shuffled;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int[] param_1 = obj.reset();
+ * int[] param_2 = obj.shuffle();
+ */
