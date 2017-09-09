8ec1a84d270bf40c0de23557788990fe54a9f8b4
justWarlow <31121824+justWarlow@users.noreply.github.com>
2017-09-09 08:12:59 -0400

    09/09/2017

---
+++ 62.java
@@ -0,0 +1,1 @@
+class Solution {    public int uniquePaths(int m, int n) {    int[] map = new int[Math.min(m, n)];    map[0] =1;    int pre = 0;    for (int i = 0; i < Math.max(m, n); i++) {        pre = 0;        for (int j = 0; j < Math.min(m, n); j++) {            map[j] += pre;            pre = map[j];        }    }    return map[Math.min(m, n) - 1];        }}
