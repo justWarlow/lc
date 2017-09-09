64118751f9249c2ad026c09e856078dc20411f46
justWarlow <31121824+justWarlow@users.noreply.github.com>
2017-09-09 08:23:45 -0400

    Update 191.java

--- 191.java
+++ 191.java
@@ -1,12 +1,12 @@
 public class Solution {
     public int hammingWeight(int n) {
-		int sum = 0;
-		while (n != 0) {
-			if ((n & 1) == 1) {
-				sum++;
-			}
-			n = n >>> 1;
-		}
-		return sum;
+        int sum = 0;
+        while (n != 0) {
+            if ((n & 1) == 1) {
+                sum++;
+            }
+            n = n >>> 1;
 	}
+	return sum;
+    }
 }
