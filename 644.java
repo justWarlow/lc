b08b8659fa3a84fece8a2a43ae888e06bc48ced3
justWarlow <31121824+justWarlow@users.noreply.github.com>
2017-09-10 01:02:03 -0400

    Update 644.java

--- 644.java
+++ 644.java
@@ -1,1 +1,14 @@
-class Solution {    public int strangePrinter(String s) {        int[][] dp = new int[s.length()][s.length()];        for (int i = s.length() - 1; i >= 0; i--) {            for (int j = i; j < s.length(); j++) {                dp[i][j] = j - i + 1;                for (int k = i; k < j; k++) {                    dp[i][j] = Math.min(dp[i][j], dp[i][k] + dp[k + 1][j] - (s.charAt(k) == s.charAt(j) ? 1 : 0));                }            }        }        return s.isEmpty() ? 0 : dp[0][s.length() - 1];    }}
+class Solution {
+    public int strangePrinter(String s) {
+        int[][] dp = new int[s.length()][s.length()];
+        for (int i = s.length() - 1; i >= 0; i--) {
+            for (int j = i; j < s.length(); j++) {
+                dp[i][j] = j - i + 1;
+                for (int k = i; k < j; k++) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i][k] + dp[k + 1][j] - (s.charAt(k) == s.charAt(j) ? 1 : 0));
+                }
+            }
+        }
+        return s.isEmpty() ? 0 : dp[0][s.length() - 1];
+    }
+}
