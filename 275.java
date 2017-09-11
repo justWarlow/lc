58d72cf10fa1a9e67bee3353a88e0a4cb71f322e
justWarlow <31121824+justWarlow@users.noreply.github.com>
2017-09-11 03:22:34 -0400

    11/09/2017

---
+++ 275.java
@@ -0,0 +1,1 @@
+class Solution {    public int hIndex(int[] citations) {        int n = citations.length;        int low = 0, high = n;        while(low < high) {        int mid = (low + high + 1) / 2;        if (citations[n - mid] < mid) {            high = mid - 1;        } else {            low = mid;        }    }    return low;    }}
---
+++ 461.java
@@ -0,0 +1,1 @@
+class Solution {    public int hammingDistance(int x, int y) {        int z = x ^ y;        int dist = 0;        while (z != 0) {          dist++;          z &= (z - 1);        }        return dist;      }}
