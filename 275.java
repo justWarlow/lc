ad9b117ebf28323660362d95751d9957be9ba642
justWarlow <31121824+justWarlow@users.noreply.github.com>
2017-09-11 03:23:15 -0400

    Update 275.java

--- 275.java
+++ 275.java
@@ -1,1 +1,15 @@
-class Solution {    public int hIndex(int[] citations) {        int n = citations.length;        int low = 0, high = n;        while(low < high) {        int mid = (low + high + 1) / 2;        if (citations[n - mid] < mid) {            high = mid - 1;        } else {            low = mid;        }    }    return low;    }}
+class Solution {
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        int low = 0, high = n;
+        while(low < high) {
+        int mid = (low + high + 1) / 2;
+        if (citations[n - mid] < mid) {
+            high = mid - 1;
+        } else {
+            low = mid;
+        }
+    }
+    return low;
+    }
+}
