f4ea6f90a415e8f8a0dfdd0f41cce32417425a00
justWarlow <31121824+justWarlow@users.noreply.github.com>
2017-09-11 03:34:18 -0400

    Update 572.java

--- 572.java
+++ 572.java
@@ -1,1 +1,30 @@
-/** * Definition for a binary tree node. * public class TreeNode { *     int val; *     TreeNode left; *     TreeNode right; *     TreeNode(int x) { val = x; } * } */class Solution {    public boolean isSubtree(TreeNode s, TreeNode t) {        if (s == null) {            return false;        }        if (helper(s,t)) {            return true;        }        return isSubtree(s.left, t) || isSubtree(s.right, t);    }        public boolean helper(TreeNode s, TreeNode t){        if (s == null && t == null) {            return true;        }        if (s == null || t == null) {            return false;        }        return s.val == t.val && helper(s.left, t.left) && helper(s.right, t.right);        }}
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if (s == null) {
+            return false;
+        }
+        if (helper(s,t)) {
+            return true;
+        }
+        return isSubtree(s.left, t) || isSubtree(s.right, t);
+    }
+    
+    public boolean helper(TreeNode s, TreeNode t){
+        if (s == null && t == null) {
+            return true;
+        }
+        if (s == null || t == null) {
+            return false;
+        }
+        return s.val == t.val && helper(s.left, t.left) && helper(s.right, t.right);    
+    }
+}
