cbc967e358aa069e1c0f0bffc3692441dd3cee6d
justWarlow <31121824+justWarlow@users.noreply.github.com>
2017-09-11 03:29:39 -0400

    Update 71.java

--- 71.java
+++ 71.java
@@ -1,1 +1,24 @@
-class Solution {    public String simplifyPath(String path) {        String[] elems = path.split("/");	    LinkedList<String> stack = new LinkedList<>();	    for (int i = 0; i < elems.length; i++ ) {	        switch (elems[i]) {	        	case ""  : break;	        	case "." : break;		        case "..": if (!stack.isEmpty() ) {                               stack.pop();                           } break;	        	default  : stack.push("/" + elems[i]);	        }	    }	    if (stack.isEmpty()) {            stack.push("/");        }   	StringBuffer sb = new StringBuffer();	while (!stack.isEmpty()) {        sb.insert(0, stack.pop());    }   	return sb.toString();        }}
+class Solution {
+    public String simplifyPath(String path) {
+        String[] elems = path.split("/");
+        LinkedList<String> stack = new LinkedList<>();
+	      for (int i = 0; i < elems.length; i++ ) {
+	          switch (elems[i]) {
+                case ""  : break;
+	            	case "." : break;
+                case "..": if (!stack.isEmpty() ) {
+                                   stack.pop();
+                           } break;
+                default  : stack.push("/" + elems[i]);
+	          }
+        }
+	      if (stack.isEmpty()) {
+              stack.push("/");
+        }
+        StringBuffer sb = new StringBuffer();
+        while (!stack.isEmpty()) {
+            sb.insert(0, stack.pop());
+        }
+        return sb.toString();    
+    }
+}
