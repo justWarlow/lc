6d5c65cf6a56e5809556a31d03425f9587eec468
justWarlow <31121824+justWarlow@users.noreply.github.com>
2017-09-11 03:39:33 -0400

    Update 71.java

--- 71.java
+++ 71.java
@@ -2,18 +2,18 @@ class Solution {
     public String simplifyPath(String path) {
         String[] elems = path.split("/");
         LinkedList<String> stack = new LinkedList<>();
-	    for (int i = 0; i < elems.length; i++ ) {
-	        switch (elems[i]) {
-                    case ""  : break;
-	            case "." : break;
-                    case "..": if (!stack.isEmpty() ) {
-                                   stack.pop();
-                               } break;
-                    default  : stack.push("/" + elems[i]);
-	        }
-            }
-	    if (stack.isEmpty()) {
-            stack.push("/");
+	for (int i = 0; i < elems.length; i++ ) {
+	    switch (elems[i]) {
+                case ""  : break;
+	        case "." : break;
+                case "..": if (!stack.isEmpty() ) {
+                               stack.pop();
+                           } break;
+                default  : stack.push("/" + elems[i]);
+	    }
+        }
+	if (stack.isEmpty()) {
+        stack.push("/");
         }
         StringBuffer sb = new StringBuffer();
         while (!stack.isEmpty()) {
